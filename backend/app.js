f6287d3d371b6820890c7a20ce55a02490cf0a01
Mdsparks17 <Mdsparks17@gmail.com>
2024-06-07 14:29:20 -0700

    Understanding CORS

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use('/api/posts', (req, res, next) => {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
+    next();
+})
+
+app.post("/api/posts", (req, res, next) => {
+    const post = req.body; 
+    console.log(post);
+    return res.status(201).json({
+        message: 'Post added successfully'
+    });
+});
+
+app.get('/api/posts', (req, res, next) => {
     const posts = [
         { id: 'asdf1234', title: "First server-side post", content: "This is coming from the server"},
         { id: 'adfasd', title: "Second server-side post", content: "This is coming from the server!"}
