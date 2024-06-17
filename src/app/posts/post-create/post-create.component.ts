9a0fa3a6bd626da90bca8eda339276c3505117e3
Mdsparks17 <Mdsparks17@gmail.com>
2024-06-17 15:04:32 -0700

    edit feature

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,26 +1,49 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { PostsService } from '../posts.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Post } from '../post.model';
 
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
   styleUrls: ['./post-create.component.css']
 })
-export class PostCreateComponent {
+export class PostCreateComponent implements OnInit{
   enteredTitle = "";
   enteredContent = "";
+  post: Post | any;
+  private mode = 'create';
+  private postId: string = "";
 
-  constructor(public postsService: PostsService) {
-    
+  constructor(public postsService: PostsService, public route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has('postId')) {
+        this.mode = 'edit';
+        let param = paramMap.get('postId');
+        this.postId = param !== null ? param : "";
+        this.post = this.postsService.getPost(this.postId);
+      } else {
+        this.mode = 'create';
+        this.postId = "";
+      }
+    });
   }
 
-  onAddPost(form: NgForm) {
+  onSavePost(form: NgForm) {
     if (form.invalid) {
       return;
     }
 
-    this.postsService.addPost(form.value.title, form.value.content);
-    form.resetForm();
+    if (this.mode === 'create') {
+      this.postsService.addPost(form.value.title, form.value.content);
+      form.resetForm();
+    } else {
+      this.postsService.updatePost(this.postId, form.value.title, form.value.content);
+    }
+
+
   }
 }
